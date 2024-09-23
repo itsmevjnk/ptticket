3b4cf5f257ec76aea5efedaa24c0eebd32bd8796
Thanh Vinh Nguyen <ngtv0404@gmail.com>
2024-09-23 13:05:23 +1000

    add SQL initialisation for authorisation key store

--- sql_setup/init.sql
+++ sql_setup/init.sql
@@ -5,5 +5,7 @@ DROP GROUP IF EXISTS "ptticket";
 CREATE GROUP "ptticket";
 DROP USER IF EXISTS "static";
 DROP USER IF EXISTS "dynamic";
+DROP USER IF EXISTS "auth";
 CREATE USER "static" ENCRYPTED PASSWORD 'ptticket_static' IN GROUP "ptticket";
 CREATE USER "dynamic" ENCRYPTED PASSWORD 'ptticket_dynamic' IN GROUP "ptticket";
+CREATE USER "auth" ENCRYPTED PASSWORD 'ptticket_auth' IN GROUP "ptticket";
