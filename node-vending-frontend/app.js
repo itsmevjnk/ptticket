7c9875a5c64c1eceaf8f2398d652b193b3377e3e
Thanh Vinh Nguyen <ngtv0404@gmail.com>
2024-09-26 02:55:05 +1000

    add ticket querying

--- node-vending-frontend/app.js
+++ node-vending-frontend/app.js
@@ -14,6 +14,7 @@ const app = express();
 app.use(express.static(__dirname + '/static'));
 app.use('/bs', express.static(__dirname + '/node_modules/bootstrap/dist')); // export Bootstrap for use
 app.use('/jq', express.static(__dirname + '/node_modules/jquery/dist')); // export JQuery too
+app.use('/qr', express.static(__dirname + '/node_modules/html5-qrcode')); // QR code reader
 
 const cors = require('cors');
 app.use(cors());
@@ -40,6 +41,60 @@ app.get('/fareTypes', (req, res) => {
 app.get('/products', (req, res) => {
     axios.get(DB_API + '/products?hideZones=true&dict=true', options).then((resp) => {
         respondHttp(res, resp.status, resp.data.message);
+    });
+});
+
+app.get('/card/:id', (req, res) => {
+    axios.get(DB_API + `/cards/qr/${req.params.id}`, options).then((resp) => {
+        if (resp.status != 200) return respondHttp(res, resp.status, resp.data.message);
+        let payload = {
+            disabled: resp.data.message.disabled
+        };
+        if (!payload.disabled) {
+            payload.balance = resp.data.message.balance / 100;
+            payload.status = (resp.data.message.touchedOn != 0) ? 'Touched on' : 'Touched off';
+            let prodExpiry = new Date(resp.data.message.prodValidated); prodExpiry.setMinutes(prodExpiry.getMinutes() + resp.data.message.prodDuration);
+            payload.productExpiry = prodExpiry.toISOString();
+
+            Promise.all([
+                axios.get(DB_API + `/tickets/${resp.data.message.ticketID}/passes`, options),
+                axios.get(DB_API + `/products/${resp.data.message.currentProduct}`, options),
+                axios.get(DB_API + `/fareTypes/${resp.data.message.fareType}?hideFares=true`, options)
+            ]).then((respArray) => {
+                /* product name */
+                let resp = respArray[1];
+                payload.product = (resp.status != 200) ? `Error querying product (${resp.status})` : resp.data.message.name;
+
+                /* fare type name */
+                resp = respArray[2];
+                payload.fareType = (resp.status != 200) ? `Error querying fare type (${resp.status})` : resp.data.message;
+
+                /* passes */
+                resp = respArray[0];
+                if (resp.status != 200) {
+                    payload.passes = null;
+                    respondHttp(res, 200, payload);
+                }
+                else {
+                    payload.passes = [];
+                    let promises = [];
+                    for (let pass of resp.data.message) {
+                        payload.passes.push({
+                            // product: pass.product,
+                            duration: pass.duration,
+                            activeDate: pass.activationDate
+                        });
+                        promises.push(axios.get(DB_API + `/products/${pass.product}`, options));
+                    }
+                    Promise.all(promises).then((respArray) => {
+                        for (let i = 0; i < respArray.length; i++)
+                            payload.passes[i].product = (respArray[i].status != 200) ? `Error querying product (${respArray[i].status})` : respArray[i].data.message.name;
+                        respondHttp(res, 200, payload);
+                    });
+                }
+
+            });
+        } else return respondHttp(res, 200, payload);
     });
 });
 
